f888207fe16391ad01c8b40e8811a8e838b37c61
samsuzie <samriddhisharma710@gmail.com>
2025-08-05 16:00:36 +0530

    feat: added sign-in and sign-up UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+// this file and its content will only be used for rendering the routes and not for developing the code . 
+// the actual code will be developed or coded in sign-in-view.tsx file 
+import { Card } from "@/components/ui/card";
+import { Sign } from "crypto";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+const Page=()=>{
+    return<SignInView />
+}
+export default Page;
+
+// to find the above page , we have to go to
+// http://localhost:3000/sign-in
+
