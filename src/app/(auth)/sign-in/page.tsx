8781d2f247182173deac6224668587740fd35d13
samsuzie <samriddhisharma710@gmail.com>
2025-08-15 14:17:16 +0530

    05:authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,17 @@
 import { Card } from "@/components/ui/card";
 import { Sign } from "crypto";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
-const Page=()=>{
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+const Page=async()=>{
+    const session = await  auth.api.getSession({
+    headers: await headers(),
+    });
+
+    if(!!session){
+        redirect('/');
+    }
     return<SignInView />
 }
 export default Page;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -6,12 +6,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import{Form, FormField, FormItem, FormLabel, FormControl, FormMessage} from "@/components/ui/form";
-import { useRouter } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card";
 import {Alert,AlertDescription, AlertTitle} from "@/components/ui/alert";
 import { OctagonAlertIcon } from "lucide-react";
 import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import {FaGithub, FaGoogle} from "react-icons/fa";
 
 
 const formSchema = z.object({
@@ -40,11 +41,34 @@ export const SignInView = ()=>{
         {
             email:data.email,
             password:data.password,
+            callbackURL:"/",
         },
         {
             onSuccess:()=>{
                 setPending(false);
                 router.push("/");
+            },
+            onError: ({error})=>{
+                setPending(false);
+               setError(error.message) 
+            },
+
+        }
+       );     
+    };
+
+    const onSocial = (provider:"github"|"google")=>{
+       setError(null);
+       setPending(true);
+        authClient.signIn.social(
+        {
+           provider:provider,
+           callbackURL:"/",
+        },
+        {
+            onSuccess:()=>{
+                setPending(false);
+                
             },
             onError: ({error})=>{
                 setPending(false);
@@ -128,11 +152,11 @@ export const SignInView = ()=>{
 
                 
                 <div className="grid grid-cols-2 gap-4">
-                    <Button disabled={pending} variant="outline" type="button" className="w-full">
-                        Google
+                    <Button disabled={pending} onClick={()=>onSocial("google")}  variant="outline" type="button" className="w-full">
+                        <FaGoogle/>
                         </Button>
-                        <Button disabled={pending} variant="outline" type="button" className="w-full">
-                        GitHub
+                        <Button disabled={pending} onClick={()=>onSocial("github")} variant="outline" type="button" className="w-full">
+                         <FaGithub/>
                         </Button>
                 </div>
                 <div className="text-center text-sm">
