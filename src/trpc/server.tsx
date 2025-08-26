a034363d4030e9e8f6caafe86068b84d0d052f7b
samsuzie <samriddhisharma710@gmail.com>
2025-08-26 13:50:44 +0530

    08:TRPC SETUP

---
+++ src/trpc/server.tsx
@@ -0,0 +1,18 @@
+//this is used when you want to call trpc client directly on server side
+import 'server-only'; // <-- ensure this file cannot be imported from the client
+import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
+import { cache } from 'react';
+import { createTRPCContext } from './init';
+import { makeQueryClient } from './query-client';
+import { appRouter } from './routers/_app';
+// IMPORTANT: Create a stable getter for the query client that
+//            will return the same client during the same request.
+export const getQueryClient = cache(makeQueryClient);
+export const trpc = createTRPCOptionsProxy({
+  ctx: createTRPCContext,
+  router: appRouter,
+  queryClient: getQueryClient,
+});
+//it is a shortcut used in calling trpc procedure
+export const caller = appRouter.createCaller(createTRPCContext);
+
