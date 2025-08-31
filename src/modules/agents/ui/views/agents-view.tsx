3e08e0b3f4624187b8cc1169f4de115b802befcf
samsuzie <samriddhisharma710@gmail.com>
2025-08-31 11:57:53 +0530

    10:responsive dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,12 +3,12 @@ import {  useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
-
 export const AgentsView = ()=>{
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
     return (
         <div>
+            
             {JSON.stringify(data,null,2)}
         </div>
     );
